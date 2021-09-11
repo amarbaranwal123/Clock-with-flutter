ce94351e75df179779c2d557a743221dfada12ef
Amarnath Baranwal <79193713+amarbaranwal123@users.noreply.github.com>
2021-09-11 22:48:53 +0530

    first commit

---
+++ lib/data.dart
@@ -0,0 +1,8 @@
+// import 'package:clock_app/constants/theme_data.dart';
+import 'package:clock_app/enums.dart';
+import 'models/menu_info.dart';
+
+List<MenuInfo> menuItems = [
+  MenuInfo(MenuType.clock,
+      title: 'Clock', imageSource: 'assets/clock_icon.png'),
+];
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:clock_app/enums.dart';
+import 'package:clock_app/models/menu_info.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:provider/provider.dart';
+import 'views/homepage.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChangeNotifierProvider<MenuInfo>(
+        create: (context) => MenuInfo(MenuType.clock),
+        child: HomePage(),
+      ),
+    );
+  }
+}
